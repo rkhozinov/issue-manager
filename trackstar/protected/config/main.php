ec623efee417736cfeb22ab2e3b230ae8bfa628f
Ruslan <fallrabble@gmail.com>
2013-05-03 02:27:45 +0400

    some chages in config file

--- trackstar/protected/config/main.php
+++ trackstar/protected/config/main.php
@@ -8,7 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Issue Manager',
-	'id'=>'TrackStar',
+	'id'=>'Issue Manager',
 	'theme'=>'newtheme',
 	//'language'=>'rev',
 
