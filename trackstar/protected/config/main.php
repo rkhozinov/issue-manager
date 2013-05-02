7c7fa26cd71aee3222a4db9ce40e442935efba1e
Ruslan <fallrabble@gmail.com>
2013-05-03 02:26:28 +0400

    some chages in config file

--- trackstar/protected/config/main.php
+++ trackstar/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'TrackStar',
+	'name'=>'Issue Manager',
 	'id'=>'TrackStar',
 	'theme'=>'newtheme',
 	//'language'=>'rev',
