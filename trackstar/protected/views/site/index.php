3f69465a2d800d25e1911bf91c4f7731d2c39c90
Ruslan <fallrabble@gmail.com>
2013-05-05 23:21:50 +0400

    change project layout

--- trackstar/protected/views/site/index.php
+++ trackstar/protected/views/site/index.php
@@ -2,6 +2,14 @@
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
+<p> Issue Manger is a Software Development Life Cycle (SDLC) issue management application.<br/>
+    Its main goal is to help keep track of all the many issues that arise
+    throughout the course of building software applications.<br/>
+    It is a user-based application that allows the creation of user accounts and grants access to the
+    application features, once a user has been authenticated and authorized.<br/>
+    It allows a user to add and manage projects.
+</p>
+
 <?php if(!Yii::app()->user->isGuest):?>
 <p>
    You last logged in on <?php echo Yii::app()->user->lastLogin; ?>.	
