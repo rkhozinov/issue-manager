2c4a2035a15babbb5834675e328999a677f7eec4
Ruslan <fallrabble@gmail.com>
2013-05-05 23:25:02 +0400

    change project layout

--- trackstar/protected/views/site/index.php
+++ trackstar/protected/views/site/index.php
@@ -8,13 +8,13 @@
     </p>
 <?php endif;?>
 
-<p> Issue Manger is a Software Development Life Cycle (SDLC) issue management application.<br/>
+<h3> Issue Manger is a Software Development Life Cycle (SDLC) issue management application.<br/>
     Its main goal is to help keep track of all the many issues that arise
     throughout the course of building software applications.<br/>
     It is a user-based application that allows the creation of user accounts and grants access to the
     application features,<br/>
     once a user has been authenticated and authorized.<br/>
     It allows a user to add and manage projects.
-</p>
+</h3>
 
 
